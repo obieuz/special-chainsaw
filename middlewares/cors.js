f284fe7000cf12151ae6b9a6c69cca1091408425
Robert Zając <98836348+obieuz@users.noreply.github.com>
2024-05-29 08:10:43 +0200

    Update cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,7 +3,7 @@ module.exports = (req,res,next)=>{
     res.header("Access-Control-Allow-Method","GET,POST,OPTIONS");
     res.header("Access-Control-Allow-Headers","Origin,Content-Type, Accept, X-Auth-Token");
     if(req.method==="OPTIONS"){
-        res.status(204).json();
+        return res.status(204).json();
     }
     next();
 }
